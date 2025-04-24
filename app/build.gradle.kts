aabf4324d7d7397ca89029269ea44f415771e7ca
JorgeRodriguez08 <jlrodriguez.1294@gmail.com>
2025-04-24 16:56:58 -0300

    ✨ [FEATURE] Setup Room persistence layer
    - Created Item entity with Room annotations 📦
    - Defined ItemDao interface with insert, update, delete, and query methods 🧩
    - Implemented ItemDatabase abstract class extending RoomDatabase 🏗️
    - Registered singleton instance of the database using Room.databaseBuilder() 🔁

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29"
+    id("com.google.devtools.ksp") version "2.1.20-2.0.0"
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -66,8 +66,8 @@ android {
 
 dependencies {
     // Import the Compose BOM
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2025.04.01"))
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling")
@@ -80,8 +80,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
 
-    //Room
-    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
-    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
-    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+    // Room
+    implementation("androidx.room:room-runtime:2.7.1")
+    ksp("androidx.room:room-compiler:2.7.1")
+    implementation("androidx.room:room-ktx:2.7.1")
 }
